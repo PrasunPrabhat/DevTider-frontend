cf16ea677f1bbbd00e5b9a78dbff175fdb425978
PrasunPrabhat <prasun2000kir@gmail.com>
2025-09-01 01:23:54 +0530

    Loading

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -129,6 +129,24 @@ const NavBar = () => {
               Hi,{" "}
               <span className="font-semibold text-white">
                 {userData.firstName}
+                {/* ✅ Show Premium Tick if user is premium */}
+                {userData.isPreminum && (
+                  <span className="text-yellow-400 flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <span className="text-xs font-semibold">Premium</span>
+                  </span>
+                )}
               </span>
             </p>
 
@@ -147,6 +165,12 @@ const NavBar = () => {
                     alt="User profile"
                     className="object-cover w-full h-full"
                   />
+                  {/* 🔹 Premium Badge on avatar */}
+                  {userData.isPreminum && (
+                    <span className="absolute bottom-0 right-0 bg-yellow-400 text-black rounded-full p-1 border border-white">
+                      ⭐
+                    </span>
+                  )}
                 </div>
               </summary>
               <ul
